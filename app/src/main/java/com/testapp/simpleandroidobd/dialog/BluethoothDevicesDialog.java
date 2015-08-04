44145d62b0fec4c06c58ff94b9b867c034a5d712
A-PLOUIS <antonio.pierre.louis@gmail.com>
2015-08-04 15:21:44 -0400

    Modified BluethoothDevicesDialog
    - Extend DialogFragment
    - Removed useless parameters
    
    Added bluetooth permission to manifest

--- app/src/main/java/com/testapp/simpleandroidobd/dialog/BluethoothDevicesDialog.java
+++ app/src/main/java/com/testapp/simpleandroidobd/dialog/BluethoothDevicesDialog.java
@@ -1,70 +1,64 @@
 package com.testapp.simpleandroidobd.dialog;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.testapp.simpleandroidobd.R;
+
+import java.util.Set;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link BluethoothDevicesDialog.OnFragmentInteractionListener} interface
+ * {@link BluethoothDevicesDialogListener} interface
  * to handle interaction events.
  * Use the {@link BluethoothDevicesDialog#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class BluethoothDevicesDialog extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class BluethoothDevicesDialog extends DialogFragment {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private BluethoothDevicesDialogListener mListener;
 
-    private OnFragmentInteractionListener mListener;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BluethoothDevicesDialog.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BluethoothDevicesDialog newInstance(String param1, String param2) {
-        BluethoothDevicesDialog fragment = new BluethoothDevicesDialog();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private Set<BluetoothDevice> m_bluetoothDevices;
 
     public BluethoothDevicesDialog() {
         // Required empty public constructor
     }
 
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     * @return A new instance of fragment BluethoothDevicesDialog.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static BluethoothDevicesDialog newInstance() {
+        return new BluethoothDevicesDialog();
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        if (adapter != null) {
+            m_bluetoothDevices = adapter.getBondedDevices();
+        } else {
+            Toast.makeText(getActivity(), "Bluetooth not supported on device", Toast.LENGTH_LONG).show();
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bluethooth_devices_dialog, container, false);
     }
 
@@ -79,10 +73,10 @@ public class BluethoothDevicesDialog extends Fragment {
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         try {
-            mListener = (OnFragmentInteractionListener) activity;
+            mListener = (BluethoothDevicesDialogListener) activity;
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
+                    + " must implement BluethoothDevicesDialogListener");
         }
     }
 
@@ -102,9 +96,9 @@ public class BluethoothDevicesDialog extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
+    public interface BluethoothDevicesDialogListener {
         // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
+        void onFragmentInteraction(Uri uri);
     }
 
 }
