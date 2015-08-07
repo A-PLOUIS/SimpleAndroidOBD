59c7ab0a938fa7c93336db145c2641786832606e
A-PLOUIS <antonio.pierre.louis@gmail.com>
2015-08-07 11:13:50 -0400

    Added custom Application class SimpleOBDReaderApplication.

---
+++ app/src/main/java/com/testapp/simpleandroidobd/SimpleOBDReaderApplication.java
@@ -0,0 +1,14 @@
+package com.testapp.simpleandroidobd;
+
+import android.app.Application;
+
+/**
+ * Created by PIERRE-LOUIS Antonio on 07/08/2015.
+ */
+public class SimpleOBDReaderApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
