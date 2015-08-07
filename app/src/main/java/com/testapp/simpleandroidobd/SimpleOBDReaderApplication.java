6669993854a56670f3836cc25575883826ac5ed6
A-PLOUIS <antonio.pierre.louis@gmail.com>
2015-08-07 11:25:52 -0400

    Enabled parse crash reporting

--- app/src/main/java/com/testapp/simpleandroidobd/SimpleOBDReaderApplication.java
+++ app/src/main/java/com/testapp/simpleandroidobd/SimpleOBDReaderApplication.java
@@ -2,6 +2,9 @@ package com.testapp.simpleandroidobd;
 
 import android.app.Application;
 
+import com.parse.Parse;
+import com.parse.ParseCrashReporting;
+
 /**
  * Created by PIERRE-LOUIS Antonio on 07/08/2015.
  */
@@ -10,5 +13,7 @@ public class SimpleOBDReaderApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        ParseCrashReporting.enable(this);
+        Parse.initialize(this, "YO3OwgDtN6CmoUhwnA2ykyJGXqXwiZsG0nNTbTdH", "NnAzJixJV1renn5g2V5n6ZZmZOH9HUMRRGWDXFhQ");
     }
 }
